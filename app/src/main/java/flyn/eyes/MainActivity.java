bfa15c50408964a284111a7517582f14597626af
imflyn <imflyn@163.com>
2017-03-14 20:26:18 +0800

    first commit

---
+++ app/src/main/java/flyn/eyes/MainActivity.java
@@ -0,0 +1,15 @@
+package flyn.eyes;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
